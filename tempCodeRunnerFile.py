a3a2acf523c50c6852f3a97768788c7880d6619b
gamlerd13 <gamlerd13@gmail.com>
2023-08-12 23:09:22 -0500

    feat: add some features

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,15 @@
-index
+print(list(datos.items()))
+# print("\n")
+# print(datos.items())
+
+
+# # recorrer un diccionario en for
+
+# for clave, valor in datos.items():
+#     print(f"{clave} ==> {valor}")
+
+# print(type(datos.items()))
+# # print(list(datos.items()))
+
+
+
--- usar_json_otro_archivo.py
+++ usar_json_otro_archivo.py
@@ -1,22 +1,27 @@
 import json
+from pprint import pprint, pp # Pretty print
 
 # Se abre el archivo desde la ruta donde se encuentre
 with open('./datos/datos.json') as datos:
     datos = json.load(datos)
 
 
-print(list(datos.items()))
-print("\n")
-print(datos.items())
+pprint(datos, width=500, depth=2, indent=4, compact=True) # EL pprint da un estilo bonito cuando imprime en pantalla
+# pp(datos)  # Por ahora se que ordena los datos de acuerdo al abecedario
 
 
-# recorrer un diccionario en for
-
-for clave, valor in datos.items():
-    print(f"{clave} ==> {valor}")
-
-print(type(datos.items()))
 # print(list(datos.items()))
+# print("\n")
+# print(datos.items())
+
+
+# # recorrer un diccionario en for
+
+# for clave, valor in datos.items():
+#     print(f"{clave} ==> {valor}")
+
+# print(type(datos.items()))
+# # print(list(datos.items()))
 
 
 
